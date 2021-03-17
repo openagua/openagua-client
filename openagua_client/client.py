ce47b769d38bf3b6009c48aa671035a5dd741d1f
rheinheimer <herr.rhein@gmail.com>
2021-03-17 09:34:15 -0600

    minor refactoring to bypass main api functions

--- openagua_client/client.py
+++ openagua_client/client.py
@@ -24,40 +24,35 @@ class Client:
     def __getattr__(self, name):
         def method(*args, **kwargs):
             if name[:4] == 'add_':
-                return self.request_post(name, **kwargs)
+                return self.add(name[:4] + 's', **kwargs)
             elif name[:4] == 'get_':
-                return self.request_get(name, *args, **kwargs)
+                return self.get(name[:4] + 's', *args, **kwargs)
             elif name[:7] == 'update_':
-                return self.request_put(name, *args, **kwargs)
+                return self.update(name[:7] + 's', *args, **kwargs)
             else:
                 return getattr(self, name)(*args, **kwargs)
 
         return method
 
-    def request_post(self, fn, **kwargs):
-        resource = fn[4:]  # add_
-        url = '{}/{}'.format(self.api_endpoint, resource + 's')
+    def get(self, resource, *args, **kwargs):
+        resource_id = args[0]
+        url = '{}/{}/{}'.format(self.api_endpoint, resource, resource_id)
+        resp = requests.get(url, headers=self.api_headers, params=kwargs)
+        return self.prepare_response(resp)
+
+    def add(self, resource, **kwargs):
+        url = '{}/{}'.format(self.api_endpoint, resource)
         resp = requests.post(url, headers=self.api_headers, json=kwargs)
-        if resp.status_code == 200:
-            return resp.json()
-        else:
-            return {'status_code': resp.status_code}
+        return self.prepare_response(resp)
 
-    def request_get(self, fn, *args, **kwargs):
-        resource = fn[4:]  # get_
+    def update(self, resource, *args, **kwargs):
         resource_id = args[0]
-        url = '{}/{}/{}'.format(self.api_endpoint, resource + 's', resource_id)
+        url = '{}/{}/{}'.format(self.api_endpoint, resource, resource_id)
         resp = requests.get(url, headers=self.api_headers, params=kwargs)
-        if resp.status_code == 200:
-            return resp.json()
-        else:
-            return {'status_code': resp.status_code}
+        return self.prepare_response(resp)
 
-    def request_put(self, fn, *args, **kwargs):
-        resource = fn[7:]  # update_
-        resource_id = args[0]
-        url = '{}/{}/{}'.format(self.api_endpoint, resource + 's', resource_id)
-        resp = requests.get(url, headers=self.api_headers, params=kwargs)
+    @staticmethod
+    def prepare_response(resp):
         if resp.status_code == 200:
             return resp.json()
         else:
